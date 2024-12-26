72208044d5b47dff63c4d8f4574eeb7b99324243
Ekant Verma <ekantvermasolanki@gmail.com>
2024-12-26 07:07:22 +0530

    Event Loop eg3 added

---
+++ 09-libuv-eventLoop/code/eventLoop3.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+// Poll
+setImmediate(() => console.log("setImmediate"));
+
+//Timer
+setTimeout(() => console.log("Timer Expired"), 0);
+
+//Promise
+Promise.resolve().then(() => console.log("Promise"));
+
+fs.readFile("file.txt", "utf8", () => {
+    setTimeout(() => console.log("2nd Timer"), 0);
+    Promise.resolve().then(() => console.log("2nd promise"));
+    process.nextTick(() => console.log("2nd nextTick"));
+    setImmediate(() => console.log("2nd setImmediate"));
+    console.log("File Read");
+})
+// Next Tick
+process.nextTick(() => console.log("nextTick"));
+
+console.log("last line of file");
+
+
+// output: 
+// End of this file
+// nextTick
+// Promise
+// Set Time out
+// Set Immidiate
+// File read
+// 2nd nextTick
+// 2nd promise
+// 2nd setImmediate
+// 2nd Timer
