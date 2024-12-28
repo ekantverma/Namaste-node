287cf302ec9b274ad0e64e6ff38530faf48c93a8
Ekant Verma <ekantvermasolanki@gmail.com>
2024-12-28 17:49:26 +0530

    Notes added

--- 10-EventLoop-threadPool/code/EventLoop-thread.js
+++ 10-EventLoop-threadPool/code/EventLoop-thread.js
@@ -1,1 +1,10 @@
-// Explore V8 repository and libuv library docs
+// Explore V8 repository and libuv library docs(libuv/src/unix/core)
+
+// Node js is  single threading or multithreading ?
+//  |
+//  |
+//  --> it is both when need as per as sync and async processes.
+//  --> it has max default threads count 4.
+//  --> if more process are coming symultaneously then it will create new threads / or wait in queue for exmpty first thread
+//  --> it is called as thread pool.
+//  --> You can change the count of threads in thread pool by setting the environment variable UV_THREADPOOL_SIZE.
