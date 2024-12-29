ae01825376e9ec2783740d733e8dc26185fb0181
Ekant Verma <ekantvermasolanki@gmail.com>
2024-12-29 14:20:06 +0530

    Created first server

---
+++ 11-Creating-a-server/code/server.js
@@ -0,0 +1,15 @@
+// Creating a server
+const http = require('node:http');
+
+const server = http.createServer(function(req, res) {
+    // reply to them
+    if(req.url === '/data'){
+        res.end("data");
+    }
+    res.end("Hello Server ji!")
+
+});
+
+server.listen(8080);
+
+// This method not for production server
